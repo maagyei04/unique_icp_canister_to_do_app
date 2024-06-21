54f9ef31763b7011e097e10d7e562660359f8423
SavageeAddie <nikhilkumarak_cse20@its.edu.in>
2024-06-21 13:52:26 +0530

    Added routes

--- src/index.ts
+++ src/index.ts
@@ -18,16 +18,16 @@ export default Server(() => {
     app.use(express.json());
 
     app.post("/todo", (req, res) => {
-        const todo: Todo = { 
-            id: uuidv4(), 
-            title: req.body.title, 
-            description: req.body.description, 
-            completed: false, 
-            createdAt: getCurrentDate(), 
-            updatedAt: null 
+        const todo: Todo = {
+            id: uuidv4(),
+            title: req.body.title,
+            description: req.body.description,
+            completed: false,
+            createdAt: getCurrentDate(),
+            updatedAt: null,
         };
         todoStorage.insert(todo.id, todo);
-        res.json(todo);
+        res.status(201).json(todo);
     });
 
     app.get("/todos", (req, res) => {
@@ -38,7 +38,7 @@ export default Server(() => {
         const todoId = req.params.id;
         const todoOpt = todoStorage.get(todoId);
         if ("None" in todoOpt) {
-            res.status(404).send(`To-Do item with id=${todoId} not found`);
+            res.status(404).json({ error: `To-Do item with id=${todoId} not found` });
         } else {
             res.json(todoOpt.Some);
         }
@@ -48,7 +48,7 @@ export default Server(() => {
         const todoId = req.params.id;
         const todoOpt = todoStorage.get(todoId);
         if ("None" in todoOpt) {
-            res.status(404).send(`To-Do item with id=${todoId} not found`);
+            res.status(404).json({ error: `To-Do item with id=${todoId} not found` });
         } else {
             const todo = todoOpt.Some;
             todo.title = req.body.title ?? todo.title;
@@ -64,16 +64,37 @@ export default Server(() => {
         const todoId = req.params.id;
         const deletedTodo = todoStorage.remove(todoId);
         if ("None" in deletedTodo) {
-            res.status(404).send(`To-Do item with id=${todoId} not found`);
+            res.status(404).json({ error: `To-Do item with id=${todoId} not found` });
         } else {
             res.json(deletedTodo.Some);
         }
     });
 
+    // New Route: Mark a todo as completed
+    app.put("/todo/:id/complete", (req, res) => {
+        const todoId = req.params.id;
+        const todoOpt = todoStorage.get(todoId);
+        if ("None" in todoOpt) {
+            res.status(404).json({ error: `To-Do item with id=${todoId} not found` });
+        } else {
+            const todo = todoOpt.Some;
+            todo.completed = true;
+            todo.updatedAt = getCurrentDate();
+            todoStorage.insert(todoId, todo);
+            res.json(todo);
+        }
+    });
+
+    // New Route: Get all completed todos
+    app.get("/todos/completed", (req, res) => {
+        const completedTodos = todoStorage.values().filter(todo => todo.completed);
+        res.json(completedTodos);
+    });
+
     return app.listen();
 });
 
-function getCurrentDate() {
+function getCurrentDate(): Date {
     const timestamp = new Number(ic.time());
     return new Date(timestamp.valueOf() / 1000_000);
 }
